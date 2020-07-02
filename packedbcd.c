51a4011f3a4d718b28e39f89292274354598175a
akuseryuinc <36928833+Dahzek@users.noreply.github.com>
2020-07-02 17:16:52 -0400

    Packed BCD
    Packed BCD

---
+++ packedbcd.c
@@ -0,0 +1,72 @@
+//gcc 7.4.0
+
+#include <stdio.h>
+
+int main(void)
+{
+    //Declaracion de variables
+    int numero = 0, copianum = 0, digito =  0, copiadigito = 0, cont_bits = 0, longitud = 0, cont = 0, contPotencia = 1, contBinario = 0, pow2 = 0, potencia = 1;
+    char bcd[41];
+    
+    scanf("%i", &numero); //Lee el numero a transformar
+    
+    copianum = numero; //Copia el numero, para trabajar con este
+    
+    CalcularLongitud:
+    if(copianum != 0){
+       longitud++;
+       copianum/=10;
+       goto CalcularLongitud;
+    } 
+    
+    copianum = numero;
+    
+    printf("Numero: %i\n", numero);
+    printf("Longitud: %i\n", longitud);
+    
+    cont = longitud-1;
+    
+    CalcularPotencia: //Calculo x cuanto tendre que dividir para obtener el primer digito
+    if(contPotencia <= cont){
+        potencia *= 10;
+        contPotencia++;
+        goto CalcularPotencia;
+    }
+    
+    TransformarBCD:
+    if(potencia > 0){
+        digito = copianum/potencia; //Consigo el primer digito
+        copiadigito = digito;
+        
+        pow2 = 8; //Setea contBinario en 0
+        contBinario = 0; //Setea contBinario en 0
+        
+        Binario: //Pasamos el numero a binario
+        if(contBinario <= 3){
+            if(digito >= pow2){
+                bcd[cont_bits] = '1';
+                digito -= pow2;
+            }
+            
+            else {
+                bcd[cont_bits] = '0';
+            }
+            
+            pow2 /= 2;
+            cont_bits++;
+            contBinario++;
+            goto Binario;
+        }
+        
+        copianum -= copiadigito*potencia; //Elimino el primer numero
+        potencia /= 10; //Calculo x cuanto tendre que dividir para obtener el primer digito
+       
+        goto TransformarBCD;
+    }
+    
+    bcd[cont_bits] =  '\0'; //Añade un caracter nulo a la cadena
+   
+    printf("Numero expresado en BCD\n%s\n", bcd);
+   
+    return 0;
+}

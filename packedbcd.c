c61b140e12860c6d09d83280e86479c658cb9b2d
akuseryuinc <36928833+Dahzek@users.noreply.github.com>
2020-07-02 20:16:32 -0400

    Update packedbcd.c

--- packedbcd.c
+++ packedbcd.c
@@ -1,4 +1,5 @@
 //gcc 7.4.0
+//Version con entero
 //Desarrollado por @akuseryuinc
 
 #include <stdio.h>
@@ -10,24 +11,24 @@ int main(void)
     char bcd[41];
     
     scanf("%i", &numero); //Lee el numero a transformar
+    if(numero == 0){
+        longitud = 1;
+        goto CalcularPotencia;
+    }
     
     copianum = numero; //Copia el numero, para trabajar con este
     
-    CalcularLongitud: //Calcula la longitud del numero ingresado
+    CalcularLongitud:
     if(copianum != 0){
        longitud++;
        copianum/=10;
        goto CalcularLongitud;
-    } 
+    }
     
     copianum = numero;
-    
-    printf("Numero: %i\n", numero);
-    printf("Longitud: %i\n", longitud);
-    
     cont = longitud-1;
     
-    CalcularPotencia: //Calcula x cuanto tendre que dividir para obtener el primer digito
+    CalcularPotencia: //Calculo x cuanto tendre que dividir para obtener el primer digito
     if(contPotencia <= cont){
         potencia *= 10;
         contPotencia++;
@@ -35,45 +36,40 @@ int main(void)
     }
     
     TransformarBCD:
-    //Si hay digitos por transformar, recorre el "ciclo"
     if(potencia > 0){
         digito = copianum/potencia; //Consigo el primer digito
         copiadigito = digito;
         
-        pow2 = 8; //Setea pow2 en 8 (2 elevado a 3)
+        pow2 = 8; //Setea contBinario en 0
         contBinario = 0; //Setea contBinario en 0
         
         Binario: //Pasamos el numero a binario
         if(contBinario <= 3){
-            //Si el digito es mayor a 2 elevado a X se asigna un 1 y se le resta 2^X
             if(digito >= pow2){
                 bcd[cont_bits] = '1';
                 digito -= pow2;
             }
-            //Si no, se asigna un 0
+            
             else {
                 bcd[cont_bits] = '0';
             }
             
-            pow2 /= 2; //La potencia disminuye en 1 a su exponente
+            pow2 /= 2;
             cont_bits++;
             contBinario++;
-
             goto Binario;
         }
         
-        //Añade una separacion x nible
-        bcd[cont_bits] = ' ';
-        cont_bits++;
-        
         copianum -= copiadigito*potencia; //Elimino el primer numero
         potencia /= 10; //Calculo x cuanto tendre que dividir para obtener el primer digito
-                 
+       
         goto TransformarBCD;
     }
     
     bcd[cont_bits] =  '\0'; //Añade un caracter nulo a la cadena
-   
+    
+    printf("Numero: %i\n", numero);
+    printf("Longitud: %i\n", longitud);
     printf("Numero expresado en BCD\n%s\n", bcd);
    
     return 0;
